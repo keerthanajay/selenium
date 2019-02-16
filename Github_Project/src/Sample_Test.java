bb99be30cb12dcd332c8b512c5127171e9bd6b88
keeru <keeru@keerthanas-mbp.attlocal.net>
2019-02-15 20:19:03 -0600

    added new line

--- Github_Project/src/Sample_Test.java
+++ Github_Project/src/Sample_Test.java
@@ -4,6 +4,7 @@ public class Sample_Test {
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 System.out.println("sample Github test");
+System.out.println("intial commit");
 	}
 
 }
